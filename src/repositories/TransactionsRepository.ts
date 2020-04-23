21a94c217ca8c4db7e1c255210b97fafbf9f43ad
unknown <Administrator@TRJ3573393.wks.internal.timbrasil.com.br>
2020-04-23 01:11:56 -0300

    terminado o desafio 5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, currentValue) => {
+      if (currentValue.type === 'income') return acc + currentValue.value;
+      return acc;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acc, currentValue) => {
+      if (currentValue.type === 'outcome') return acc + currentValue.value;
+      return acc;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
